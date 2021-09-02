5096e1cb8a9ffc39136a90da51a99eb4bd779eca
Anderson <andersonribeiro.si@hotmail.com>
2021-09-02 19:37:21 -0300

    Upload de imagens

--- App.js
+++ App.js
@@ -1,19 +1,97 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Image, Button, Platform, ActivityIndicator } from 'react-native';
+import Firebase from 'firebase'
+import * as ImagePicker from 'expo-image-picker'
+import { firebaseConfig } from './firebase';
 
 export default function App() {
+
+  if(!Firebase.apps.length){
+    Firebase.initializeApp(firebaseConfig)
+  }
+
+  const [image, setImage] = useState("")
+  const [uploading, setUploading] = useState(false)
+
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== 'web') {
+        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== 'granted') {
+          alert('Desculpe, sem permissão para a câmera')
+        }
+      }
+    })()
+  }, [])
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.all,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1
+    })
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri)
+    }
+  }
+
+  const uploadImage = async () => {
+    const blob = await new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.onload = function () {
+        resolve(xhr.response);
+      };
+
+      xhr.onerror = function () {
+        reject(new TypeError('Network request failed'));
+      };
+
+      xhr.responseType = 'blob';
+      xhr.open('GET', image, true);
+      xhr.send(null);
+    });
+
+    const ref = Firebase.storage().ref().child(new Date().toISOString())
+    const snapshot = ref.put(blob);
+
+    snapshot.on(
+      Firebase.storage.TaskEvent.STATE_CHANGED,
+      () => {
+        setUploading(true)
+      },
+      (error) => {
+        setUploading(false)
+        console.log(error);
+        // blob.close();
+        return
+      },
+      () => {
+        snapshot.snapshot.ref.getDownloadURL().then((url) => {
+          setUploading(false)
+          console.log("download url: ", url);
+          // blob.close()
+          return url
+        })
+      }
+    )
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={{ uri: image }} style={{ width: 300, height: 300 }} />
+      <Button title="choose picture" onPress={pickImage} />
+      {!uploading ? <Button title="upload" onPress={uploadImage} /> : <ActivityIndicator size="large" color="#000" />}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
---
+++ firebase.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBFr0NG_nKTcQ5NMJtLNN6EnzdWn1nKQVg",
+    authDomain: "sl7-bets-f90af.firebaseapp.com",
+    databaseURL: "https://sl7-bets-f90af-default-rtdb.firebaseio.com",
+    projectId: "sl7-bets-f90af",
+    storageBucket: "sl7-bets-f90af.appspot.com",
+    messagingSenderId: "1067161593647",
+    appId: "1:1067161593647:web:8b8971cc3265f9c35ba3da"
+  };
